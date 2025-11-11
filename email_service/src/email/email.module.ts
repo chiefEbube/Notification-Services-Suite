2366aa693964b284728108e6435f9c3fba51a614
chiefEbube <ebubechi.anyanwu@yahoo.com>
2025-11-11 20:32:33 +0100

    feat(email_service): Implement caching and circuit breaker for email processing
    - Added Dockerfile for building the email service with Node.js.
    - Updated package.json and package-lock.json to include new dependencies: @nestjs/cache-manager, cache-manager-redis-store, and opossum for caching and circuit breaker functionality.
    - Refactored EmailService to utilize caching for user and template data, improving performance and reducing external service calls.
    - Integrated circuit breaker pattern using opossum to enhance reliability when fetching templates from the template service.
    - Adjusted module imports for proper path resolution.

--- email_service/src/email/email.module.ts
+++ email_service/src/email/email.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { EmailService } from './email.service';
 import { EmailController } from './email.controller';
 import { HttpModule } from '@nestjs/axios';
-import { SendgridModule } from 'src/sendgrid/sendgrid.module';
+import { SendgridModule } from '../sendgrid/sendgrid.module';
 
 @Module({
   imports: [HttpModule, SendgridModule],
